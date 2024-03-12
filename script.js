e0d561843a8b019816296ac7d568eb760a74685a
Joep2307 <Joep.groenteman@gmail.com>
2024-03-12 12:10:01 +0100

    first

---
+++ script.js
@@ -0,0 +1,30 @@
+var woorden = [
+    'galgje',
+    'amsterdam',
+    'hond',
+    'kat',
+    'laptop',
+    'bier'
+]
+var resultaat = 0
+var gekozenWoord = ''
+var aantalGok = ''
+var aantalFouten = ''
+var woord = document.querySelector('#woord')
+var gok = document.querySelector('#gok')
+var Fouten = document.querySelector('fouten')
+
+
+function KiesWoord() {
+    gekozenWoord = woorden[Math.floor(math.random()* woorden.length)];
+}
+
+function maakKeuzes(){
+
+}
+
+function maakFouten() {
+
+}
+
+
