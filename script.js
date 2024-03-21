7f7b52c44a183a776ecf0f63b67470fff64f58c9
Joep2307 <Joep.groenteman@gmail.com>
2024-03-21 10:35:49 +0100

    Final

--- script.js
+++ script.js
@@ -1,47 +1,68 @@
-// Maak woordenlijst waar woord uit gekozen wordt
+// Maak woordenlijst waar woord uit gekozen wordt. Chatgpt gebruikt om deze woorden lijst op alfabetische volgorde te zetten 
 const woorden = [
-    "Array",
-    "constant",
-    "javascript",
+    "array",
+    "article",
+    "body",
+    "button",
+    "class",
     "coding",
     "console",
+    "constant",
+    "css",
+    "document",
+    "display",
+    "div",
+    "footer",
+    "function",
+    "github",
+    "header",
+    "html",
+    "javascript",
+    "lapot",
+    "margin",
+    "padding",
+    "repository",
+    "script",
+    "section",
+    "simon",
+    "src"
 
-];
+]
 
 // Variabelen maken
-let antwoord = '';
-let maxFout = 12;
-let fouten = 0;
-let gokken = [];
-let woordStatus = 0;
+let antwoord = ''
+let maxFout = 12
+let fouten = 0
+let gokken = []
+let woordStatus = 0
 
 // Random woord kiezen
 function randomWoord() {
-    antwoord = woorden[Math.floor(Math.random() * woorden.length)];
+    antwoord = woorden[Math.floor(Math.random() * woorden.length)]
 }
 
-// Letters maken; gevonden op yyoutube. Ik kan de video niet terug vinden
+// Letters maken; gevonden op youtube. Ik kan de video niet terug vinden
 function maakKnoppen() {
     let knopHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter => `
         <button class="keyboard-btn" id="${letter}" onClick="checkGok('${letter}')">${letter}</button>
-    `).join('');
-    document.getElementById('keyboard').innerHTML = knopHTML;
+    `).join('')
+    document.getElementById('keyboard').innerHTML = knopHTML
 }
 
 // Kijken of de gok goed is; hulp van W3school "https://www.w3schools.com/jsref/jsref_indexof.asp"
 function checkGok(gekozenLetter) {
     if (gokken.indexOf(gekozenLetter) === -1) {
-        gokken.push(gekozenLetter);
-        document.getElementById(gekozenLetter).setAttribute('disabled', true);
+        gokken.push(gekozenLetter)
+        document.getElementById(gekozenLetter).setAttribute('disabled', true)
 
         if (antwoord.indexOf(gekozenLetter) >= 0) {
-            gegoktWoord();
-            hebJeGewonnen();
+            gegoktWoord()
+            hebJeGewonnen()
         } else if (antwoord.indexOf(gekozenLetter) === -1) {
-            fouten++;
-            updateFouten();
-            hebJeVerloren();
-            updateMannetje();
+            fouten++
+            updateFouten()
+            updateMannetje()
+            hebJeVerloren()
             console.log(fouten)
         }
     }
@@ -49,26 +70,27 @@ function checkGok(gekozenLetter) {
 
 // Update hangman bij een fout
 function updateMannetje() {
-    document.getElementById('mannetje').src = './images/galgje-tekeningen/mannetje-' + fouten + '.png';
+    document.getElementById('mannetje').src = './images/galgje-tekeningen/mannetje-' + fouten + '.png'
 }
 
 //Gegokt woord checken; hulp van https://codepen.io/takaneichinose/pen/yVXYRo
 function gegoktWoord() {
-    woordStatus = antwoord.split('').map(letter => (gokken.indexOf(letter) >= 0 ? letter : " _ ")).join('');
-    document.getElementById('wordSpotlight').innerHTML = woordStatus;
+    woordStatus = antwoord.split('').map(letter => (gokken.indexOf(letter) >= 0 ? letter : " _ ")).join('')
+    document.getElementById('wordSpotlight').innerHTML = woordStatus
 }
 
 // Kijk of je gewonnen hebt
 function hebJeGewonnen() {
     if (woordStatus === antwoord) {
-        alert("Gefeliciteerd, je hebt gewonnen!");
+        alert("KUT je hebt gewonnen :(")
     }
 }
 
 // Kijk of je verloren hebt
 function hebJeVerloren() {
     if (fouten === maxFout) {
-        alert("Je hebt verloren. Het juiste woord was: " + antwoord);
+        alert("haha je hebt verloren! hoe wist je niet dat het: " + antwoord + " was :)")
+        reset()
     }
 }
 
@@ -80,17 +102,27 @@ function updateFouten() {
 function reset() {
     fouten = 0
     gokken = []
-    document.getElementById('mannetje').src = './images/galgje-tekeningen/mannetje-0.jpg';
+    document.getElementById('mannetje').src = './images/galgje-tekeningen/mannetje-0.png'
 
-    randomWoord();
-    maakKnoppen();
-    gegoktWoord();
+    randomWoord()
+    maakKnoppen()
+    gegoktWoord()
+
+    window.location.reload()
 }
 
-document.getElementById('maxfout').innerHTML = maxFout;
+addEventListener('keydown', ({ key }) => {
+    switch (key) {
+        case 'Enter':
+            reset()
+            break
+    }
+})
 
-randomWoord();
-maakKnoppen();
-gegoktWoord();
+document.getElementById('maxfout').innerHTML = maxFout
+
+randomWoord()
+maakKnoppen()
+gegoktWoord()
 
 
